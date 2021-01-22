c81ec17c5b10557daa41cc377f67f04707b3dea6
sabiwara <sabiwara@gmail.com>
2021-01-22 12:32:32 +0900

    Fix broken benchmark

--- bench/vector/vector_reverse.exs
+++ bench/vector/vector_reverse.exs
@@ -1,8 +1,8 @@
 # TODO bench bigger numbers
 list = Enum.to_list(1..100)
-vector = Vector.new(list)
+vector = A.Vector.new(list)
 
 Benchee.run(%{
-  "Vector.reverse/1" => fn -> Vector.reverse(vector) end,
+  "A.Vector.reverse/1" => fn -> A.Vector.reverse(vector) end,
   "Enum.reverse/2" => fn -> Enum.reverse(list) end
 })
