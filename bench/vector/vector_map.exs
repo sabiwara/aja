1828b9436d2d79f8d803c7cad36ec81d68de18ce
sabiwara <sabiwara@gmail.com>
2021-02-20 15:02:21 +0900

    Fix benches

--- bench/vector/vector_map.exs
+++ bench/vector/vector_map.exs
@@ -2,11 +2,9 @@
 list = Enum.to_list(1..100)
 array = :array.from_list(list)
 vector = A.Vector.new(list)
-raw = A.Vector.Raw.new(list)
 
 Benchee.run(%{
   ":array.map/2" => fn -> :array.map(fn _, i -> i + 1 end, array) end,
   "A.Vector.map/2" => fn -> A.Vector.map(vector, &(&1 + 1)) end,
-  "A.Vector.Raw.map/2" => fn -> A.Vector.Raw.map(raw, &(&1 + 1)) end,
   "Enum.map/2" => fn -> Enum.map(list, &(&1 + 1)) end
 })
--- bench/vector/vector_slice.exs
+++ bench/vector/vector_slice.exs
@@ -1,10 +1,7 @@
 list = Enum.to_list(1..100)
 vector = A.Vector.new(list)
-raw = A.Vector.Raw.new(list)
 
 Benchee.run(%{
-  # XXX the API is different for Raw (start, last) != (start, length)
-  "A.Vector.Raw.slice/3" => fn -> A.Vector.Raw.slice(raw, 10, 90) end,
   "A.Vector.slice/3 (vector)" => fn -> A.Vector.slice(vector, 10, 80) end,
   "Enum.slice/3 (vector)" => fn -> Enum.slice(vector, 10, 80) end,
   "Enum.slice/3 (list)" => fn -> Enum.slice(list, 10, 80) end
