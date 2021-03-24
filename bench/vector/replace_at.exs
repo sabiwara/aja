3a3e2bafee450a913e994bc69838ea6b6833ad0d
sabiwara <sabiwara@gmail.com>
2021-03-24 22:31:33 +0900

    Benchmark for different sizes

--- bench/vector/replace_at.exs
+++ bench/vector/replace_at.exs
@@ -1,9 +1,22 @@
-list = Enum.to_list(1..100)
-array = :array.from_list(list)
-vector = A.Vector.new(list)
+inputs =
+  Enum.map([100, 101, 1_000, 1_001, 10_000, 10_001], fn n ->
+    {"n = #{n}", n}
+  end)
 
-Benchee.run(%{
-  ":array.set/3" => fn -> :array.set(55, :foo, array) end,
-  "A.Vector.replace_at/3" => fn -> A.Vector.replace_at(vector, 55, :foo) end,
-  "List.replace_at/3" => fn -> List.replace_at(list, 55, :foo) end
-})
+Benchee.run(
+  %{
+    ":array.set/3" =>
+      {fn {array, i} -> :array.set(i, :foo, array) end,
+       before_scenario: fn n ->
+         {1..n |> Enum.to_list() |> :array.from_list(), div(n, 2)}
+       end},
+    "A.Vector.replace_at/3" =>
+      {fn {vector, i} -> A.Vector.replace_at(vector, i, :foo) end,
+       before_scenario: fn n -> {A.Vector.new(1..n), div(n, 2)} end},
+    "List.replace_at/3" =>
+      {fn {list, i} -> List.replace_at(list, i, :foo) end,
+       before_scenario: fn n -> {Enum.to_list(1..n), div(n, 2)} end}
+  },
+  inputs: inputs,
+  time: 2
+)
