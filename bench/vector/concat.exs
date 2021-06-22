db321c5c67415bcacd5b60542d4ff52b8cf8d1b4
sabiwara <sabiwara@gmail.com>
2021-06-22 09:49:06 +0900

    Benchmark vector-vector concat

---
+++ bench/vector/concat.exs
@@ -0,0 +1,23 @@
+import A
+
+small_list = Enum.to_list(1..100)
+big_list = Enum.to_list(1..1000)
+small_vector = A.Vector.new(small_list)
+big_vector = A.Vector.new(big_list)
+
+Benchee.run(
+  %{
+    "big_vector +++ small_list" => fn ->
+      big_vector +++ small_list
+    end,
+    "big_vector +++ small_vector" => fn ->
+      big_vector +++ small_vector
+    end,
+    "small_vector +++ big_vector" => fn ->
+      small_vector +++ big_vector
+    end,
+    "small_list ++ big_list" => fn -> small_list ++ big_list end,
+    "big_list ++ small_list" => fn -> big_list ++ small_list end
+  },
+  memory_time: 2
+)
